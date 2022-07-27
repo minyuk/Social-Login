c87591ae2962e001d9fe1317e886daa4180fb0a6
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-07-27 14:46:58 +0900

    project commit

---
+++ src/main/java/com/social/oauthlogin/OauthloginApplication.java
@@ -0,0 +1,13 @@
+package com.social.oauthlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/oauthlogin/OauthloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.oauthlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
